2b04378dcdc3f132677c83d22be1e3c2daa309b6
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2021-10-20 14:51:55 -0400

    Remove Key Check logic from logic.py

--- services/sendgrid/send_email_form/app.py
+++ services/sendgrid/send_email_form/app.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template, request
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-from logic import check_api_tokens, send_sendgrid_email
+from logic import send_sendgrid_email
 
 ## Configuration:
 ## In Accepted Tokens, provide the NAMES of keys in .secrets
 ## that you want to have access to your tool.
-accepted_tokens = ['DerricksToken1234', 'NiharsToken1234']
+accepted_tokens = ['derrick_token']
 
 from_email = 'Derrick+testing@wayscript.com'
 
@@ -23,16 +23,16 @@ def index():
 @app.route('/send_email', methods = ['GET', 'POST'])
 def form_submit():
     if request.method == 'POST':
-        to_email        = request.form.get('to_email')
-        subject         = request.form.get('subject')
-        content_to_send = request.form.get('content_to_send')
-        api_token       = request.form.get('api_token')
+        to_email        = request.json.get('to_email')
+        subject         = request.json.get('subject')
+        content_to_send = request.json.get('content_to_send')
+        api_token       = str(request.json.get('api_token'))
 
-        if check_api_tokens(api_token, accepted_tokens):
+        if api_token in accepted_tokens:
             response = send_sendgrid_email(from_email, to_email, subject, content_to_send)
-            return response
+            return 'success!'
         else:
-            return 'Invalid API Key'
+            return 'Invalid Token - Contact System Admin'
     else:
         return 'Not a valid request, Please Send POST.'
 
--- services/sendgrid/send_email_form/logic.py
+++ services/sendgrid/send_email_form/logic.py
@@ -3,19 +3,6 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-# Check API Keys
-def check_api_tokens(provided_token, accepted_tokens):
-    provided_token_value = os.environ.get(str(provided_token))
-    if isinstance( accepted_tokens, list ):
-        accepted_tokens = [os.environ.get(token) for token in accepted_tokens]
-        if provided_token in accepted_tokens:
-            return True
-    else:
-        if provided_token_value == os.environ.get(accepted_tokens):
-            return True
-        else:
-            return False
-
 def send_sendgrid_email(from_email, to_email, subject, html_content):
     message = Mail(
         from_email = str(from_email),
